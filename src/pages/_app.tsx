6c5ab38e3d96216790d8e6f4fc8efcfd4f665b49
EnanDorta <junior.goncalves01@hotmail.com>
2022-06-16 13:27:30 -0300

    Create next-app and config chakra-ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Theme } from '../styles/theme';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={Theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
