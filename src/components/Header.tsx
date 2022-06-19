53e2f897e94367828bae8600ed184deabc3340f6
EnanDorta <junior.goncalves01@hotmail.com>
2022-06-19 12:18:17 -0300

    finalizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header () {
   return (
@@ -45,6 +45,28 @@ export function Header () {
           _placeholder={{color: 'gray.400'}}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack 
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Enan Dorta</Text>
+            <Text color="gray.300" fontSize="small">junior.goncalves01@hotmail.com</Text>
+          </Box>
+          <Avatar size="md" name="Enan Dorta" src="https://github.com/enandorta.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
